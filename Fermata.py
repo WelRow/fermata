2a5a7b7e4a3fc0bb817fa77ffbb7f31df6790cd2
Josias <jkkanyinda@gmail.com>
2023-07-19 21:22:00 -0400

    Changes to Main
    - Corrected 'trebel' to 'treble' since it created an error when trying to convert notes from one clef to treble.
    - Fixed an issue where selecting a clef that doesn't have a 3rd octave will cause an error when converting notes.
    - Fixed an issue where having no note selected after changing your major will crash the program
    - Changed 'Fermata Alpha v1.0.0' to 'Fermata v1.0.0'

--- Fermata.py
+++ Fermata.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+import os.path as path
 from notes import Notes
 
 user_control = sg.Column([
@@ -7,7 +8,7 @@ user_control = sg.Column([
     [sg.Combo(['A', 'B', 'C', 'D', 'E', 'F', 'G'], default_value='C', key='-NOTES-')],
     [sg.Combo(['1st', '2nd', '3rd'], default_value='1st', key='-OCTAVES-')],
     [sg.HorizontalSeparator()],
-    [sg.Combo(['Bass Clef', 'Alto Clef', 'Tenor Clef', 'Trebel Clef'], default_value='Alto Clef', key='-DESIRED_CLEF-')],
+    [sg.Combo(['Bass Clef', 'Alto Clef', 'Tenor Clef', 'Treble Clef'], default_value='Alto Clef', key='-DESIRED_CLEF-')],
     [sg.Button('CONVERT!', key='convert')],
     [sg.Button('Credits', key='-CREDITS-')]
 ])
@@ -18,39 +19,55 @@ desired_note_image = sg.Column([[sg.Image("note_placeholder.png", key='-IMAGE2-'
 
 layout = [[user_control, user_note_image, arrow, desired_note_image]]
 
-window = sg.Window('Fermata ALPHA v1.0.0', layout, use_custom_titlebar = True, titlebar_icon='Fermata_Logo.png')
+window = sg.Window('Fermata v1.0.0', layout, use_custom_titlebar = True, titlebar_icon='Fermata_Logo.png')
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=50)
     if event == sg.WIN_CLOSED:
         break
 
-    #updates notes when the major changes
+    # creating clef, major, and clef_switch variables
+    if len(values['-CLEFS-']) == 9:
+        clef = values['-CLEFS-'][0:4].lower()
+
+    elif len(values['-CLEFS-']) == 10:
+        clef = values['-CLEFS-'][0:5].lower()
+
+    else:
+        clef = values['-CLEFS-'][0:6].lower()
+
+    
+    if len(values['-DESIRED_CLEF-']) == 9:
+        clef_switch = values['-DESIRED_CLEF-'][0:4].lower()
+
+    elif len(values['-DESIRED_CLEF-']) == 10:
+        clef_switch = values['-DESIRED_CLEF-'][0:5].lower()
+
+    else:
+        clef_switch = values['-DESIRED_CLEF-'][0:6].lower()
+    
+
+    major = values['-MAJORS-'][0].lower() + "_major"
+
+    
+    # When the major changes...
     if event == '-MAJORS-':
+        # it'll update the Notes
         window['-NOTES-'].update(values=Notes(values['-MAJORS-']))
+        window['-NOTES-'].update(set_to_index = 0)
+
+        # if major doesnt have a low 3 octave note, then it only displays 2 avaliable octaves
+        if not path.isfile(f"clef_notes\\{clef}\\{major}\\3_{values['-NOTES-'][0].lower()}" + ".png"):
+            window['-OCTAVES-'].update(values = ['1st', '2nd'])
+            window['-OCTAVES-'].update(set_to_index = 0)
+
+        else:
+            window['-OCTAVES-'].update(values = ['1st', '2nd', '3rd'])
+            window['-OCTAVES-'].update(set_to_index = 0)
 
     #creating a file path with users input from user_control
     if event == 'convert':
-        if len(values['-CLEFS-']) == 9:
-            clef = values['-CLEFS-'][0:4].lower()
-
-        elif len(values['-CLEFS-']) == 10:
-            clef = values['-CLEFS-'][0:5].lower()
-
-        else:
-            clef = values['-CLEFS-'][0:6].lower()
-
         
-        if len(values['-DESIRED_CLEF-']) == 9:
-            clef_switch = values['-DESIRED_CLEF-'][0:4].lower()
-
-        elif len(values['-DESIRED_CLEF-']) == 10:
-            clef_switch = values['-DESIRED_CLEF-'][0:5].lower()
-
-        else:
-            clef_switch = values['-DESIRED_CLEF-'][0:6].lower()
-        
-        major = values['-MAJORS-'][0].lower() + "_major"
         note = values['-NOTES-'][0].lower()
         octave = values['-OCTAVES-'][0]
 
